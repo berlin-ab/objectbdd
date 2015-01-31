5e3539980c259eeb29583a3664e8029e2814e57d
Adam Berlin <berlin.ab@gmail.com>
2015-01-30 22:04:49 -0500

    First working distribution, v0.1.1

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ source /usr/local/bin/virtualenvwrapper.sh &&
   workon objectbdd-test &&
   pip install -r requirements.txt --upgrade --force-reinstall &&
   flake8 objectbdd &&
-  nosetests tests
+  nosetests tests &&
+  python setup.py sdist
 
